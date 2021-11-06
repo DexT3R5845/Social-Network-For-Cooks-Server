5f44aacce77dc5eb72a20baabb393d83b55935c8
vladsurhai <vlasurgay@gmail.com>
2021-11-06 19:56:52 +0200

    check ci and heroku

--- src/main/java/com/edu/netc/bakensweets/BakeNSweetsApplication.java
+++ src/main/java/com/edu/netc/bakensweets/BakeNSweetsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BakeNSweetsApplication {
-
     public static void main (String[] args) {
         SpringApplication.run(BakeNSweetsApplication.class, args);
     }
-
 }
