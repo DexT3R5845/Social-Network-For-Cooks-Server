c6cdaa45bf03f3aef0e15b0af5494cadfc786003
vladsurhai <vlasurgay@gmail.com>
2021-11-07 17:34:44 +0200

    trying my own branch

--- src/main/java/com/edu/netc/bakensweets/UserController.java
+++ src/main/java/com/edu/netc/bakensweets/UserController.java
@@ -1,6 +1,6 @@
 package com.edu.netc.bakensweets;
 
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
