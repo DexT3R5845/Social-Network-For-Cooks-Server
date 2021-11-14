47fc64fd45770ed9b732371c2cc1b92638fce581
DexT3R5845 <valentin2747@gmail.com>
2021-11-15 00:27:50 +0200

    Add Swagger2

--- src/main/java/com/edu/netc/bakensweets/security/WebSecurityConfig.java
+++ src/main/java/com/edu/netc/bakensweets/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -40,8 +41,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     http.exceptionHandling().accessDeniedPage("/login");
 
     http.apply(new JwtTokenFilterConfigurer(jwtTokenProvider));
+  }
 
-    // http.httpBasic();
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().antMatchers("/v3/api-docs/**")
+            .antMatchers("/swagger-resources/**")
+            .antMatchers("/swagger-ui.html")//
+            .antMatchers("/configuration/**")
+            .antMatchers("/webjars/**")
+            .antMatchers("/public");
   }
 
   @Bean
